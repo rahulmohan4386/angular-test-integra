b81f668b9fc65d98442dae4ce1d7b4cc7c226e16
RAHUL\rahul <rahulambadiyil@gmail.com>
2022-09-15 19:44:41 +0530

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,9 @@ import { HttpService } from './service/http.service';
 })
 export class AppComponent implements OnInit {
   title = 'http-client';
+  searchKeyword = '';
+  text =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tellus odio, posuere quis accumsan at, aliquet in nibh. Morbi tempor rhoncus hendrerit. Sed gravida porttitor congue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec rhoncus mi eget semper cursus. Morbi lectus odio, ullamcorper sit amet dignissim a, faucibus sit amet risus. In tincidunt ultricies ex, sit amet vehicula purus blandit nec.';
   data: {
     id: string;
     name: string;
